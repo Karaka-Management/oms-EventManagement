9a15b5c7381642d4467de8cb1d703903136c277e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['EventManagement' => [
+    'Account' => '',
+    'Actual' => '',
+    'Amount' => '',
+    'Budget' => '',
+    'CoreData' => '',
+    'Description' => '',
+    'Elements' => '',
+    'End' => '',
+    'Event' => '',
+    'EventManagement' => '',
+    'Events' => '',
+    'Exponential' => '',
+    'Files' => '',
+    'Group' => '',
+    'Info' => '',
+    'Linear' => '',
+    'Log' => '',
+    'Manager' => '',
+    'Manual' => '',
+    'Name' => '',
+    'People' => '',
+    'Permissions' => '',
+    'Progress' => '',
+    'Responsibility' => '',
+    'Start' => '',
+    'Status' => '',
+    'Tasks' => '',
+    'Title' => '',
+    'User' => '',
+    'UserGroup' => '',
+]];
