288821dd1710f91adce6fe0faee50910df755733
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:38 +0000

    Add test cases for null models

--- tests/Models/NullEventAttributeTest.php
+++ tests/Models/NullEventAttributeTest.php
@@ -23,7 +23,7 @@ final class NullEventAttributeTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\EventManagement\Models\NullEventAttribute
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullEventAttributeTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\EventManagement\Models\NullEventAttribute
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullEventAttributeTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\EventManagement\Models\NullEventAttribute
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
