7acf4d1e13a186840b97341574b77d18653f9ed8
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-01-02 23:34:17 +0000

    update

--- Models/PermissionCategory.php
+++ Models/PermissionCategory.php
@@ -17,7 +17,7 @@ namespace Modules\EventManagement\Models;
 use phpOMS\Stdlib\Base\Enum;
 
 /**
- * Permision state enum.
+ * Permission category enum.
  *
  * @package Modules\EventManagement\Models
  * @license OMS License 2.0
